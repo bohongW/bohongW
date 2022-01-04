eecd7cf9e193aaa4587d4e30fe1d7d403c0e656f
hc6461281 <1065125321@qq.com>
2022-01-04 17:25:51 +0800

    test

--- op/ntp/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-timedatectl set-timezone Asia/Shanghai && \
-rpm -ivh http://mirrors.wlnmp.com/centos/wlnmp-release-centos.noarch.rpm && \
-yum install -y wntp && \
-ntpdate ntp.aliyun.com
